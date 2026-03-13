0b814bc6edc50797c2e3c45055a1564588e68811
rkarimov17949 <150791906+rkarimov17949@users.noreply.github.com>
2026-03-13 16:10:25 +0400

    Update models.py

--- otp/models.py
+++ otp/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 
 class OTP(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    code = models.CharField(max_length=6)
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=8)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
