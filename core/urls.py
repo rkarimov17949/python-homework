6794f1214f3aa808098b1bb945a3d915f1589446
rkarimov17949 <rkarimov17949@ada.edu.az>
2026-03-26 23:42:30 +0400

    fix missing import BookStatsView

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import BookStatsView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),
+    path("books/stats/", BookStatsView.as_view()),
 ]
